5fbd822ec4036815d051eb6ca72b4f331e39bcdb
TheFait <35749207+TheFait@users.noreply.github.com>
2018-10-10 20:01:33 -0500

    Full Card List
    Scraper now uses a full card list to also show cards you don't own.

--- scraper.js
+++ scraper.js
@@ -1,4 +1,161 @@
-let cardList = {};
+let cardList = {
+	"Embryo #026": {standard:0, backer:0, limited:0},
+	"Zeeter": {standard:0, backer:0, limited:0},
+	"Embryo #052": {standard:0, backer:0, limited:0},
+	"Embryo #054": {standard:0, backer:0, limited:0},
+	"Embryo #056": {standard:0, backer:0, limited:0},
+	"Embryo #057": {standard:0, backer:0, limited:0},
+	"Embryo #058": {standard:0, backer:0, limited:0},
+	"Embryo #099": {standard:0, backer:0, limited:0},
+	"Embryo #062": {standard:0, backer:0, limited:0},
+	"Embryo #063": {standard:0, backer:0, limited:0},
+	"Embryo #064": {standard:0, backer:0, limited:0},
+	"Embryo #065": {standard:0, backer:0, limited:0},
+	"Embryo #073": {standard:0, backer:0, limited:0},
+	"Embryo #074": {standard:0, backer:0, limited:0},
+	"Embryo #079": {standard:0, backer:0, limited:0},
+	"Embryo #080": {standard:0, backer:0, limited:0},
+	"Embryo #081": {standard:0, backer:0, limited:0},
+	"Embryo #086": {standard:0, backer:0, limited:0},
+	"Embryo #087": {standard:0, backer:0, limited:0},
+	"Embryo #088": {standard:0, backer:0, limited:0},
+	"Embryo #106": {standard:0, backer:0, limited:0},
+	"Embryo #109": {standard:0, backer:0, limited:0},
+	"Embryo #120": {standard:0, backer:0, limited:0},
+	"Embryo #122": {standard:0, backer:0, limited:0},
+	"Embryo #123": {standard:0, backer:0, limited:0},
+	"Embryo #133": {standard:0, backer:0, limited:0},
+	"Embryo #134": {standard:0, backer:0, limited:0},
+	"Embryo #136": {standard:0, backer:0, limited:0},
+	"Embryo #138": {standard:0, backer:0, limited:0},
+	"Burrrnn": {standard:0, backer:0, limited:0},
+	"Fire-Maw": {standard:0, backer:0, limited:0},
+	"Cerberus": {standard:0, backer:0, limited:0},
+	"Enrager": {standard:0, backer:0, limited:0},
+	"Zhampion": {standard:0, backer:0, limited:0},
+	"Azzazzin": {standard:0, backer:0, limited:0},
+	"Banshee": {standard:0, backer:0, limited:0},
+	"Sparky": {standard:0, backer:0, limited:0},
+	"BurZt": {standard:0, backer:0, limited:0},
+	"Quazi": {standard:0, backer:0, limited:0},
+	"Modo": {standard:0, backer:0, limited:0},
+	"Golem": {standard:0, backer:0, limited:0},
+	"Mountain": {standard:0, backer:0, limited:0},
+	"Gargantua": {standard:0, backer:0, limited:0},
+	"Cherno-bill": {standard:0, backer:0, limited:0},
+	"Bouncer": {standard:0, backer:0, limited:0},
+	"Tiny": {standard:0, backer:0, limited:0},
+	"Znowman": {standard:0, backer:0, limited:0},
+	"Spiker": {standard:0, backer:0, limited:0},
+	"IgneouZ": {standard:0, backer:0, limited:0},
+	"Blocker": {standard:0, backer:0, limited:0},
+	"Hardy": {standard:0, backer:0, limited:0},
+	"Pyrite": {standard:0, backer:0, limited:0},
+	"Firewall": {standard:0, backer:0, limited:0},
+	"Wazte": {standard:0, backer:0, limited:0},
+	"Pit": {standard:0, backer:0, limited:0},
+	"Crater": {standard:0, backer:0, limited:0},
+	"Stapler": {standard:0, backer:0, limited:0},
+	"Nail Bomb": {standard:0, backer:0, limited:0},
+	"Goo Bottles": {standard:0, backer:0, limited:0},
+	"Chainsaw": {standard:0, backer:0, limited:0},
+	"Molotov": {standard:0, backer:0, limited:0},
+	"Bat": {standard:0, backer:0, limited:0},
+	"Item #041": {standard:0, backer:0, limited:0},
+	"Whistle": {standard:0, backer:0, limited:0},
+	"Item #069": {standard:0, backer:0, limited:0},
+	"Goo Beaker": {standard:0, backer:0, limited:0},
+	"Junk Spear": {standard:0, backer:0, limited:0},
+	"Item #072": {standard:0, backer:0, limited:0},
+	"Item #112": {standard:0, backer:0, limited:0},
+	"Extinguisher": {standard:0, backer:0, limited:0},
+	"Lawnmower": {standard:0, backer:0, limited:0},
+	"Item #115": {standard:0, backer:0, limited:0},
+	"Item #116": {standard:0, backer:0, limited:0},
+	"Fresh Meat": {standard:0, backer:0, limited:0},
+	"Item #118": {standard:0, backer:0, limited:0},
+	"Item #119": {standard:0, backer:0, limited:0},
+	"Boomstick": {standard:0, backer:0, limited:0},
+	"Tainted Goo": {standard:0, backer:0, limited:0},
+	"Corrupted Goo": {standard:0, backer:0, limited:0},
+	"Whizpar": {standard:0, backer:0, limited:0},
+	"Pushhh": {standard:0, backer:0, limited:0},
+	"Ztormmcaller": {standard:0, backer:0, limited:0},
+	"ZeuZ": {standard:0, backer:0, limited:0},
+	"Rockky": {standard:0, backer:0, limited:0},
+	"Bolderr": {standard:0, backer:0, limited:0},
+	"Walley": {standard:0, backer:0, limited:0},
+	"Pyromaz": {standard:0, backer:0, limited:0},
+	"Cynderman": {standard:0, backer:0, limited:0},
+	"Azuraz": {standard:0, backer:0, limited:0},
+	"Shroom": {standard:0, backer:0, limited:0},
+	"Vindrom": {standard:0, backer:0, limited:0},
+	"Shammann": {standard:0, backer:0, limited:0},
+	"Puffer": {standard:0, backer:0, limited:0},
+	"Poizom": {standard:0, backer:0, limited:0},
+	"Ghoul": {standard:0, backer:0, limited:0},
+	"Izze": {standard:0, backer:0, limited:0},
+	"Jetter": {standard:0, backer:0, limited:0},
+	"Freezzee": {standard:0, backer:0, limited:0},
+	"Tzunamy": {standard:0, backer:0, limited:0},
+	"Mind Flayer": {standard:0, backer:0, limited:0},
+	"Wheezy": {standard:0, backer:0, limited:0},
+	"Earthshaker": {standard:0, backer:0, limited:0},
+	"Werezomb": {standard:0, backer:0, limited:0},
+	"Z-Virus": {standard:0, backer:0, limited:0},
+	"Yggdrazil": {standard:0, backer:0, limited:0},
+	"Sapper": {standard:0, backer:0, limited:0},
+	"Keeper": {standard:0, backer:0, limited:0},
+	"Zpitter": {standard:0, backer:0, limited:0},
+	"Zhatterer": {standard:0, backer:0, limited:0},
+	"Soothsayer": {standard:0, backer:0, limited:0},
+	"Buffer": {standard:0, backer:0, limited:0},
+	"Dragger": {standard:0, backer:0, limited:0},
+	"Zyclone": {standard:0, backer:0, limited:0},
+	"Defender": {standard:0, backer:0, limited:0},
+	"Bloomer": {standard:0, backer:0, limited:0},
+	"EverlaZting": {standard:0, backer:0, limited:0},
+	"Healz": {standard:0, backer:0, limited:0},
+	"Rainz": {standard:0, backer:0, limited:0},
+	"RelentleZZ": {standard:0, backer:0, limited:0},
+	"Polluter": {standard:0, backer:0, limited:0},
+	"Zludge": {standard:0, backer:0, limited:0},
+	"Zub-Zero": {standard:0, backer:0, limited:0},
+	"Blizzard": {standard:0, backer:0, limited:0},
+	"Breezee": {standard:0, backer:0, limited:0},
+	"Zhocker": {standard:0, backer:0, limited:0},
+	"Ztorm Shield": {standard:0, backer:0, limited:0},
+	"Zquall": {standard:0, backer:0, limited:0},
+	"Draft": {standard:0, backer:0, limited:0},
+	"MonZoon": {standard:0, backer:0, limited:0},
+	"Slab": {standard:0, backer:0, limited:0},
+	"Groundy": {standard:0, backer:0, limited:0},
+	"Gaea": {standard:0, backer:0, limited:0},
+	"Alpha": {standard:0, backer:0, limited:0},
+	"Volcan": {standard:0, backer:0, limited:0},
+	"Zplitter": {standard:0, backer:0, limited:0},
+	"Zeeder": {standard:0, backer:0, limited:0},
+	"Blight": {standard:0, backer:0, limited:0},
+	"Germ": {standard:0, backer:0, limited:0},
+	"Ectoplasm": {standard:0, backer:0, limited:0},
+	"Zlopper": {standard:0, backer:0, limited:0},
+	"Boomer": {standard:0, backer:0, limited:0},
+	"Zteroid": {standard:0, backer:0, limited:0},
+	"GooZilla": {standard:0, backer:0, limited:0},
+	"Vortex": {standard:0, backer:0, limited:0},
+	"Zplash": {standard:0, backer:0, limited:0},
+	"Froztbite": {standard:0, backer:0, limited:0},
+	"Zpring": {standard:0, backer:0, limited:0},
+	"Maelstrom": {standard:0, backer:0, limited:0},
+	"Fumez": {standard:0, backer:0, limited:0},
+	"Gaz": {standard:0, backer:0, limited:0},
+	"Zap": {standard:0, backer:0, limited:0},
+	"Cactuz": {standard:0, backer:0, limited:0},
+	"Zeptic": {standard:0, backer:0, limited:0},
+	"Hazzard": {standard:0, backer:0, limited:0},
+	"Ozmoziz": {standard:0, backer:0, limited:0},
+	"Geyzer": {standard:0, backer:0, limited:0}
+};
 
 function parseText()
 {
@@ -8,15 +165,15 @@ function parseText()
 
     let obj = $(test).find('div.card-body');
 	
-	console.log(obj);
-	
+	//console.log(obj);
+	$("#container").empty();
 	
     $.each(obj, function(key, value)
     {
 		let amount = Number(value.childNodes[0].innerText.substring(2));
 		let type = "";
 		
-		console.log(value.childNodes[5]);
+		//console.log(value.childNodes[5]);
 		
 		if (value.childNodes[5].innerText !== undefined)
 			type = value.childNodes[5].innerText.trim();
@@ -24,18 +181,10 @@ function parseText()
 			type = "standard";
 		
 		let name = value.childNodes[2].innerText.trim();
-        //console.log(name + ":" + type + ":" + amount);
+        console.log(name + ":" + type + ":" + amount);
 		
 		
-		if(cardList[name] !== undefined)
-		{
-			cardList[name][type] = amount;
-		}
-		else
-		{
-			cardList[name] = {"standard":0,"backer":0,"limited":0};
-			cardList[name][type] = amount;
-		}
+		cardList[name][type] = amount;
     });
 	
 	let table = $('<table />');
